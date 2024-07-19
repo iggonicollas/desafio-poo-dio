503f0dd29cfd21c00d3a8142731b39df00c55628
iggonicollas <iggonicollas@gmail.com>
2024-07-19 16:46:03 -0300

    teste de novas implementacoes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
+
+        Curso cursoJavaDev = new Curso();
+        cursoJavaDev.setTitulo("Curso Java Desenvolvedor");
+        cursoJavaDev.setDescricao("Curso para Desenvolvedores Java");
+        cursoJavaDev.setCargaHoraria(87);
+
+        Curso cursoAzure = new Curso();
+        cursoAzure.setTitulo("Curso Microsoft Azure");
+        cursoAzure.setDescricao("Curso para Desenvolvedores Azure");
+        cursoAzure.setCargaHoraria(54);
+
+
+
+        System.out.println(cursoJavaDev);
+        System.out.println(cursoAzure);
 
     }
 }
