31707da60aa8c7dcbe6291d33cf9a44f8b997c45
iggonicollas <iggonicollas@gmail.com>
2024-07-19 19:15:38 -0300

    Concluido

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,14 +17,45 @@ public class Main {
         cursoAzure.setDescricao("Curso para Desenvolvedores Azure");
         cursoAzure.setCargaHoraria(54);
 
+        Conteudo conteudo = new Curso();
+        Conteudo conteudo1 = new Mentoria();
+
+        List<String> palavras = new ArrayList<>();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descricao mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(cursoJavaDev);
+        /*System.out.println(cursoJavaDev);
         System.out.println(cursoAzure);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Aprendizado para se tornar desenvolvedor Java");
+        bootcamp.getConteudos().add(cursoJavaDev);
+        bootcamp.getConteudos().add(cursoAzure);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devIggo = new Dev();
+        devIggo.inscreverBootcamp(bootcamp);
+        devIggo.setNome("Iggo Nicollas");
+        System.out.println("Conteudos Inscritos" + devIggo.getConteudosInscritos());
+        devIggo.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos" + devIggo.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devIggo.getConteudosConcluidos());
+
+        System.out.println("--------------------------");
+
+        Dev devCaio = new Dev();
+        devCaio.inscreverBootcamp(bootcamp);
+        devCaio.setNome("Caio Glauber");
+        System.out.println("Conteudos Inscritos" + devCaio.getConteudosInscritos());
+        devCaio.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos" + devCaio.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devCaio.getConteudosConcluidos());
     }
 }
