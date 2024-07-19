771ccc689718bed8de7f6b982105265d86d776f1
iggonicollas <iggonicollas@gmail.com>
2024-07-19 17:25:01 -0300

    etapa 1 concluida

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,10 +16,14 @@ public class Main {
         cursoAzure.setDescricao("Curso para Desenvolvedores Azure");
         cursoAzure.setCargaHoraria(54);
 
-
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descricao mentoria Java");
+        mentoria.setData(LocalDate.now());
 
         System.out.println(cursoJavaDev);
         System.out.println(cursoAzure);
+        System.out.println(mentoria);
 
     }
 }
